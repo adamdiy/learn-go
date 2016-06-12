d6880152e57630973540d9b48a39c3aec65d54bb
Don Ky <don.d.ky@gmail.com>
2016-06-11 23:30:00 -0400

    Block level scope

--- udemy/mcleod/04_scope/01_package-scope/01/main.go
+++ udemy/mcleod/04_scope/01_package-scope/01/main.go
@@ -10,8 +10,12 @@ var x int = 42
 func main() {
 	fmt.Println(x)
 	foo()
+	// This is local to main, can't be printed below
+	// block level scope
+	//y := 17
 }
 
 func foo() {
 	fmt.Println(x)
+	//fmt.Println(y)
 }
