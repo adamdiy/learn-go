74ecd4d05cc91302f60dd23720be83924a3eb1f3
Don Ky <don.d.ky@gmail.com>
2016-06-11 23:17:35 -0400

    var zero value example

---
+++ udemy/mcleod/03_variables/02_var_zero-value/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	var b string
+	var c float64
+	var d bool //by default bool = false
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
