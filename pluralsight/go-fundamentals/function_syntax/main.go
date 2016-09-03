47ed63a33bf0d4afbae30eb718251010a7c03a49
Don Ky <don.d.ky@gmail.com>
2016-09-03 09:48:35 -0400

    title case function

---
+++ pluralsight/go-fundamentals/function_syntax/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+//This is a copy of text.
+func titleCase(text string) string {
+	return text
+}
+
+func main() {
+	text := "Hello Don"
+	fmt.Println(titleCase(text))
+}
