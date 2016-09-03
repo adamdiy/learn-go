3ce5fb3a2638a20c2d8554c220732306c3d2ee69
Don Ky <don.d.ky@gmail.com>
2016-09-03 09:53:01 -0400

    Add multiple inputs, returns

--- pluralsight/go-fundamentals/function_syntax/main.go
+++ pluralsight/go-fundamentals/function_syntax/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 //This is a copy of text.
-func titleCase(text string) string {
-	return text
+func titleCase(text1, text2 string) (s1, s2 string) {
+	text1 = strings.Title(text1)
+	text2 = strings.Title(text2)
+	return text1, text2
 }
 
 func main() {
-	text := "Hello Don"
-	fmt.Println(titleCase(text))
+	text1 := "Hello Don"
+	text2 := "Hello You"
+	fmt.Println(titleCase(text1, text2))
 }
