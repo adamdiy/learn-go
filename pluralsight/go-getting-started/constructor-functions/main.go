d649682b141a73338bf3b100e321dca396fa16f9
Don Ky <don.d.ky@gmail.com>
2016-09-09 22:03:30 -0400

    constructor functions

---
+++ pluralsight/go-getting-started/constructor-functions/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//foo := &myStruct{}	
+	foo := newMyStruct()
+	foo.myMap["bar"] = "baz"
+
+	fmt.Println(foo)
+}
+
+type myStruct struct {
+	myMap map[string]string
+}
+
+func newMyStruct() *myStruct {
+	result := myStruct{}
+	result.myMap = map[string]string{}
+	return &result
+}
