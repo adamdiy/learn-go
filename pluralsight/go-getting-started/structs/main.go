95c0b6d7f7adf09e4767cb538ac4eaf06e71c30b
Don Ky <don.d.ky@gmail.com>
2016-09-11 22:24:09 -0400

    Re-working structs

--- pluralsight/go-getting-started/structs/main.go
+++ pluralsight/go-getting-started/structs/main.go
@@ -1,22 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	//foo := myStruct{}
-	//foo.myField = "bar"
-
+	//Composite literal format, cleaner way to create object.
 	//foo := myStruct{"bar"}
+	
+	//create object on the heap, this doesn't allow initializationas above so field value must be set after.
 	//foo := new(myStruct)
 	//foo := myStruct{}
+	//foo := new(myStruct)
 	//foo.myField = "bar"
 
 	foo := &myStruct{"bar"}
 
+	//println(foo.myField)
 	fmt.Println(foo)
-	println(foo.myField)
 }
 
 type myStruct struct {
 	myField string
 }
+
+
+
