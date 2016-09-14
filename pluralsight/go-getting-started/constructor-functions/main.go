c050cced3c3468c6d57f5ae3d7243321404dd3a5
Don Ky <don.d.ky@gmail.com>
2016-09-13 22:26:16 -0400

    Re-work on constructor functions

--- pluralsight/go-getting-started/constructor-functions/main.go
+++ pluralsight/go-getting-started/constructor-functions/main.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 )
 
-func main() {
-	//foo := &myStruct{}	
-	foo := newMyStruct()
-	foo.myMap["bar"] = "baz"
+//go doesn't have the concept of constructors so constructor functions are used instead.
 
+func main() {
+	//foo := &myStruct
+	foo := newMyStruct()
+	foo.myMap["bar"] = "bar"
 	fmt.Println(foo)
 }
 
 type myStruct struct {
 	myMap map[string]string
 }
+
+//constructor function starts here
 
 func newMyStruct() *myStruct {
 	result := myStruct{}
