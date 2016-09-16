1fbc59121490a773abca8c3a5c518807e4e9ac92
Don Ky <don.d.ky@gmail.com>
2016-09-15 20:51:45 -0400

    Multiple variable declaration

---
+++ pluralsight/the-go-programming-language/multiple-variable-declaration-init/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//var message string
+	//message = "Hello Go World!"
+	//var message string = "Hello World"
+	message := "Hello World"
+	
+	//var a, b, c int
+	var a, b, c int = 1, 2, 3
+
+
+	fmt.Println(message)
+	fmt.Println(a, b, c)
+}
