483a037f2fb1ab03c1d019cf8f09bfb2042085f9
Don Ky <don.d.ky@gmail.com>
2016-09-19 11:52:38 -0400

    Closures

---
+++ pluralsight/the-go-programming-language/closures/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Salutation struct {
+	name string
+	greeting string
+}
+
+//type Printer that takes in a func
+type Printer func(string) ()
+
+func Greet(salutation Salutation, do Printer) {
+	message, alternate := CreateMessage(salutation.name, salutation.greeting, "Yo", "Sup", "Ni Hao!")
+	do(message)
+	do(alternate)
+}
+
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+	message = greeting[3] + " " + name
+	alternate = "HEY!" + " " + name
+	return
+}
+
+func Print(s string) {
+	fmt.Print(s)
+}
+
+func Println(s string) {
+	fmt.Println(s)
+}
+
+func CreatePrintFunction(custom string) Printer {
+	return func(s string) {
+		fmt.Println(s + custom)
+	}
+}
+
+func main() {
+	var s = Salutation{"Don", "Hello"}
+	Greet(s, CreatePrintFunction("!!!"))
+}
