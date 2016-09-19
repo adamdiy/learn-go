96dbb19eb2e3ac7b2225dd0480c7b5fda0738e4c
Don Ky <don.d.ky@gmail.com>
2016-09-18 21:53:10 -0400

    Passing funcs

--- pluralsight/the-go-programming-language/passing-a-function/main.go
+++ pluralsight/the-go-programming-language/passing-a-function/main.go
@@ -10,11 +10,17 @@ type Salutation struct {
 }
 
 //takes a salutation object type Salutation
-func Greet(salutation Salutation) {
+//func Greet(salutation Salutation) {
+//This is so weird, we are passing Println to the "do" func
+func Greet(salutation Salutation, do func(string)) {
 	//calls CreateMessage function
 	message, alternate := CreateMessage(salutation.name, salutation.greeting, "Yo", "Sup", "Ni Hao!")
+	/*
 	fmt.Println(message)
 	fmt.Println(alternate)
+	*/
+	do(message)
+	do(alternate)
 }
 
 //greeting is now a variadic function
@@ -23,16 +29,24 @@ func CreateMessage(name string, greeting ...string) (message string, alternate s
 	fmt.Println("Len of name is:", len(name))
 	fmt.Println("Len of greeting is:", len(greeting))
 
-	fmt.Println(greeting[2])
+	fmt.Println(greeting[3])
 	fmt.Println(name)
 
-	message = greeting[2] + " " + name
+	message = greeting[3] + " " + name
 	alternate = "HEY!" + " " + name
 	return
+}
+
+func Print(s string) {
+	fmt.Print(s)
+}
+
+func Println(s string) {
+	fmt.Println(s)
 }
 
 func main() {
 	var s = Salutation{"Don", "Hello"}
 	//Here in main we pass in the s object
-	Greet(s)
+	Greet(s, Println)
 }
