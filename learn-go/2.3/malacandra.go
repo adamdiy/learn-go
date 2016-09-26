597eca84b0143382ddfa886bdbd306d47b28d78d
Don Ky <don.d.ky@gmail.com>
2016-09-25 21:21:32 -0400

    Tweaking for more clarity

--- learn-go/2.3/malacandra.go
+++ learn-go/2.3/malacandra.go
@@ -8,18 +8,23 @@ func main() {
 	const (
 		timeToArriveInDays = 28
 		secondsInDay = 86400
-
 		distanceToMalacandra = 56000000
 	)
 
-	// unknown = speed in km/s
+	// Solve for unknown speed in km/s
+	// We determine seconds
+	secondsToArrival := timeToArriveInDays * secondsInDay
+	minsToArrival := secondsToArrival / 60
+	hrsToArrival := minsToArrival / 60
+	fmt.Println("Total amount of seconds to arrival:", secondsToArrival)
+	fmt.Println("Total amount of mins to arrival:", minsToArrival)
+	fmt.Println("Total amount of hours to arrival:", hrsToArrival)
 
-	totalSeconds := timeToArriveInDays * secondsInDay
-	fmt.Println("Total amount of seconds: ",totalSeconds)
-
-	speed := distanceToMalacandra / totalSeconds
-	fmt.Println("Speed required: ", speed) // 23 km/s?
-
+	// formula for speed = distance / time
+	// speed := distanceToMalacandra / secondsToArrival
+	// since speed is measured in seconds we divide distance / seconsToArrival
+	speed := distanceToMalacandra / secondsToArrival
+	fmt.Println("Speed required to arrive in 28 days:", speed, "km/s") // 23 km/s?
 
 	//check answer:
 	fastestSpaceTravelNow := distanceToMalacandra / speed
