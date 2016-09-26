fbf371139e8a26db75a9b94db12959de79c8f1e1
Don Ky <don.d.ky@gmail.com>
2016-09-26 10:23:14 -0400

    Add rand example

---
+++ learn-go/2.4/rand.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	//pull a random number from 0-9 and add 1
+	var num = rand.Intn(10) + 1
+	fmt.Println(num)
+
+	num = rand.Intn(10) + 1
+	fmt.Println(num)
+}
