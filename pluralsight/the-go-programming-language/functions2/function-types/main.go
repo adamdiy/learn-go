8ec3831fe6a23eab979ff3c11e1c24f83aaa301c
Don Ky <don.d.ky@gmail.com>
2016-09-28 21:16:10 -0400

    Updated notes

---
+++ pluralsight/the-go-programming-language/functions2/function-types/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Salutation struct {
+	name string
+	greeting string
+}
+
+//2 - I don't even get why we need to do this? Why can't we just Println?
+func Greet(salutation Salutation) {
+	//We don't care about message so we just get rid of it
+	message, alternate := CreateMessage(salutation.name, salutation.greeting, "yo")
+	//fmt.Println(CreateMessage(salutation.name, salutation.greeting))
+	fmt.Println(message)
+	fmt.Println(alternate)
+}
+
+//3 //takes in name and greeting which are strings returns two strings
+//greeting is a variadic function
+//greeting now becomes a slice
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+	//return greeting + " " + name, "Hey!" + " " + name
+	fmt.Println(len(greeting))
+	message = greeting[1] + " " + name
+	alternate = "Hey!" + " " + name
+	return
+}
+
+//1
+func main() {
+	var s = Salutation{"Hello", "Bob"}
+	s.name = "Bob"
+	s.greeting = "Hello"
+	Greet(s)
+}
