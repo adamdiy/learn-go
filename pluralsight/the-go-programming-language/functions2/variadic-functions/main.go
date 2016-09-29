686f4cf7803bea5f407c3e655bc8f1fcb3f50d23
Don Ky <don.d.ky@gmail.com>
2016-09-28 21:14:51 -0400

    Wrap up variadic functions

--- pluralsight/the-go-programming-language/functions2/variadic-functions/main.go
+++ pluralsight/the-go-programming-language/functions2/variadic-functions/main.go
@@ -10,7 +10,7 @@ type Salutation struct {
 //2 - I don't even get why we need to do this? Why can't we just Println?
 func Greet(salutation Salutation) {
 	//We don't care about message so we just get rid of it
-	message, alternate := CreateMessage(salutation.name, salutation.greeting)
+	message, alternate := CreateMessage(salutation.name, salutation.greeting, "yo")
 	//fmt.Println(CreateMessage(salutation.name, salutation.greeting))
 	fmt.Println(message)
 	fmt.Println(alternate)
@@ -21,7 +21,8 @@ func Greet(salutation Salutation) {
 //greeting now becomes a slice
 func CreateMessage(name string, greeting ...string) (message string, alternate string) {
 	//return greeting + " " + name, "Hey!" + " " + name
-	message = greeting[0] + " " + name
+	fmt.Println(len(greeting))
+	message = greeting[1] + " " + name
 	alternate = "Hey!" + " " + name
 	return
 }
