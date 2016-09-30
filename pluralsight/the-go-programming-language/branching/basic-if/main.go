da04c2ccc44cf2c33eb418874ed889a7df48f479
Don Ky <don.d.ky@gmail.com>
2016-09-29 20:17:48 -0400

    Separated greeting into a separate function

--- pluralsight/the-go-programming-language/branching/basic-if/main.go
+++ pluralsight/the-go-programming-language/branching/basic-if/main.go
@@ -1,48 +1,9 @@
 package main
 
-import "fmt"
+import "../greeting"
 
-type Salutation struct {
-	name string
-	greeting string
-}
-
-type Printer func(string) ()
-
-func Greet(salutation Salutation, do Printer, isFormal bool) {
-	message, alternate := CreateMessage(salutation.name, salutation.greeting)
-	if isFormal {
-		do(message)
-	}
-		do(alternate)
-}
-
-func CreateMessage(name, greeting string) (message string, alternate string) {
-	message = greeting + " " + name
-	alternate = "Hey!" + " " + name
-	return
-}
-
-func Print(s string) {
-	fmt.Print(s)
-}
-
-func PrintLine(s string) {
-	fmt.Println(s)
-}
-
-func PrintCustom(s string, custom string) {
-	fmt.Println(s + custom)
-}
-func CreatePrintFunction(custom string) Printer {
-	return func(s string) {
-		fmt.Println(s + custom)
-	}
-}
 
 func main() {
-	var s = Salutation{"Hello", "Bob"}
-	s.name = "Bob"
-	s.greeting = "Hello"
-	Greet(s, CreatePrintFunction("!"), false)
+	var s = greeting.Salutation{"Bob", "Hello"}
+	greeting.Greet(s, greeting.CreatePrintFunction("!"), false)
 }
