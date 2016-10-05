a85ade5f17d81db3960389c44f77cb3f22238415
Don Ky <don.d.ky@gmail.com>
2016-10-04 22:06:51 -0400

    Use channels rather than sleep

--- pluralsight/the-go-programming-language/concurrency/main.go
+++ pluralsight/the-go-programming-language/concurrency/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"./greeting"
-	"time"
+	_"time"
 )
 
 func RenameToFrog(r greeting.Renameable) {
@@ -20,10 +20,16 @@ func main() {
 	//dereference = get the address of
 	RenameToFrog(&salutations[0])
 
+	done := make(chan bool)
+
 	//Kick off independent thread with this
-	go salutations.Greet( greeting.CreatePrintFunction("<C>"), true, 6)
+	go func() {
+		salutations.Greet( greeting.CreatePrintFunction("<C>"), true, 6)
+		done <- true
+	}()
 	salutations.Greet( greeting.CreatePrintFunction("!"), true, 6)
-	time.Sleep(100 * time.Millisecond)
+	//time.Sleep(100 * time.Millisecond)
+	<- done
 	//slice = append(slice[:1], slice[2:]... )
 	//greeting.Greet(salutations, greeting.CreatePrintFunction("!"), true, 150)
 }
