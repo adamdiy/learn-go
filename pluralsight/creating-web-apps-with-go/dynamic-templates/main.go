0a3c38ba491b4e4cf0a5b52cfaaae9a6aa268366
Don Ky <don.d.ky@gmail.com>
2016-10-10 11:37:35 -0400

    dynamic templates + static templates

---
+++ pluralsight/creating-web-apps-with-go/dynamic-templates/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		tmpl, err := template.New("test").Parse(doc)
+		if err == nil {
+			tmpl.Execute(w, req.URL.Path)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+const doc = `
+<!DOCTYPE html>
+<html>
+	<head><title>Example Title</title></head>
+	<body>
+		<h1>Hello {{.}}</h1>
+	</body>
+</htm>
+`
