b0e939804a52caa162259dcb6388fbaca2738dfb
Don Ky <don.d.ky@gmail.com>
2016-10-16 20:45:58 -0400

    Update if block to case

--- pluralsight/creating-web-apps-with-go/mvc-view/main.go
+++ pluralsight/creating-web-apps-with-go/mvc-view/main.go
@@ -16,8 +16,11 @@ func main() {
 		template := templates.Lookup(requestedFile + ".html")
 
 		var context interface{} = nil
-		if requestedFile == "home" {
+		switch requestedFile {
+		case "home":
 			context = viewmodels.GetHome()
+		case "categories":
+			context = viewmodels.GetCategories()
 		}
 		if template != nil {
 			//template.Execute(w, nil)
