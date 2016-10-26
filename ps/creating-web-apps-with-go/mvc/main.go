569c730f87525faa293d197ccb895b5fb24a5908
Don Ky <don.d.ky@gmail.com>
2016-10-25 21:46:03 -0400

    remove switch temporarily

--- ps/creating-web-apps-with-go/mvc/main.go
+++ ps/creating-web-apps-with-go/mvc/main.go
@@ -18,11 +18,16 @@ func main() {
 			templates.Lookup(requestedFile + ".html")
 
 			var context interface{} = nil
+			/*
 			switch requestedFile {
 			case "home":
 				context = viewmodels.GetHome()
 			case "categories":
 				context = viewmodels.GetCategories()
+			}
+			*/
+			if requestedFile == "home" {
+				context = viewmodels.GetHome()
 			}
 			if template != nil {
 				template.Execute(w, context)
