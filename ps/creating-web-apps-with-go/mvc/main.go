c20a7049af2c414e74b1fff38f1183e5cc29a1a3
Don Ky <don.d.ky@gmail.com>
2016-11-19 17:07:11 -0500

    Demo: Creating the Template cache

--- ps/creating-web-apps-with-go/mvc/main.go
+++ ps/creating-web-apps-with-go/mvc/main.go
@@ -4,61 +4,25 @@ import (
 	"net/http"
 	"os"
 	"text/template"
-	"bufio"
-	"strings"
-	"./viewmodels"
 )
 
 func main() {
 	templates := populateTemplates()
+
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, req *http.Request) {
-		requestedFile := req.URL.Path[1:]
-		template := 
-			templates.Lookup(requestedFile + ".html")
+			requestedFile := req.URL.Path[1:]
+			template := 
+				templates.Lookup(requestedFile + ".html")
 
-			var context interface{} = nil
-			switch requestedFile {
-			case "home":
-				context = viewmodels.GetHome()
-			case "categories":
-				context = viewmodels.GetCategories()
-			}
-			if template != nil {
-				template.Execute(w, context)
-			} else {
-				w.WriteHeader(404)
-			}
+				if template != nil {
+					template.Execute(w, nil)
+				} else {
+					w.WriteHeader(404)
+				}
 		})
-
-		http.HandleFunc("/img/", serveResource)
-		http.HandleFunc("/css/", serveResource)
-
-		http.ListenAndServe(":8000", nil)
-}
-
-func serveResource(w http.ResponseWriter, req *http.Request) {
-	path := "public" + req.URL.Path
-	var contentType string
-	if strings.HasSuffix(path, ".css") {
-		contentType = "text/css"
-	} else if strings.HasSuffix(path, ".png") {
-		contentType = "image/png"
-	} else {
-		contentType = "text/plain"
-	}
-
-	f, err := os.Open(path)
-
-	if err == nil {
-		defer f.Close()
-		w.Header().Add("Content-Type", contentType)
-
-		br := bufio.NewReader(f)
-		br.WriteTo(w)
-	} else {
-		w.WriteHeader(404)
-	}
+		//http.ListenAndServe(":8080", http.FileServer(http.Dir("public")))
+		http.ListenAndServe(":8080", nil)
 }
 
 func populateTemplates() *template.Template {
@@ -73,7 +37,8 @@ func populateTemplates() *template.Template {
 	templatePaths := new([]string)
 	for _, pathInfo := range templatePathsRaw {
 		if !pathInfo.IsDir() {
-			*templatePaths = append(*templatePaths, basePath + "/" + pathInfo.Name())
+			*templatePaths = append(*templatePaths,
+				basePath + "/" + pathInfo.Name())
 		}
 	}
 	result.ParseFiles(*templatePaths...)
